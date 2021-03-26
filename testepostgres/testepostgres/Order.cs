e5a421bff55be16aa853dff2447994146fdb35d5
Z-Maguinho-do-Piaui <klebermirandalima@gmail.com>
2021-03-26 01:41:14 -0300

    Criando o projeto para os cara

---
+++ testepostgres/testepostgres/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace testepostgres
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int iD, double latitude, double longitude, int status)
+        {
+            ID = iD;
+            Latitude = latitude;
+            Longitude = longitude;
+            Status = status;
+        }
+
+        public int ID { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+
+        // 1 - aberto; 2 - fechado
+        public int Status { get; private set; }
+    }
+}
